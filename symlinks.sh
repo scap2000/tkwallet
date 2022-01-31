0c0c9f99cb2f23ad1322e346f18b8003b1cf2bb4
scap2000 <scap2000@yahoo.com>
2022-01-31 12:49:32 -0300

    Update symlinks.sh

--- symlinks.sh
+++ symlinks.sh
@@ -3,6 +3,7 @@ ln -s ../educoin-libs/secp256k1 .
 ln -s ../educoin-libs/bitauth .
 ln -s ../educoin-libs/bitcore-lib-edu .
 ln -s ../educoin-libs/bitcore-lib-tik .
+rm -r bitcore-wallet-client
 ln -s ../educoin-libs/bitcore-wallet-client .
 cd -
 cp educoin-libs/GoogleService-Info.plist platforms/ios/TKCoin/Resources/Resources/
